35cdbddd7489eff1707596d8671cc085ca56a02e
Tom Tsui <tomtsui2642@gmail.com>
2022-01-01 23:14:12 +0900

    migrated Question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_135306) do
+ActiveRecord::Schema.define(version: 2022_01_01_141154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.text "question"
+    t.string "choice"
+    t.text "answer"
+    t.integer "score"
+    t.integer "grade"
+    t.integer "subject"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_01_01_135306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
