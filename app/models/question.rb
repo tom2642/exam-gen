c59ba67ac26c0f667c28190746f0f8576e39445d
Tom Tsui <tomtsui2642@gmail.com>
2022-01-01 23:30:57 +0900

    add enum to Question subject

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,18 @@
 class Question < ApplicationRecord
   belongs_to :user
+
+  enum subject: [
+    :Biology, :'Business, Accounting and Financial Studies',
+    :Chemistry, :Chinese, :'Chinese History', :'Chinese Literature', :'Combined Science',
+    :'Design and Applied Technology',
+    :Economics, :English, :'English Literature', :'Ethics and Religious Studies',
+    :Geography,
+    :'Health Management and Social Care', :History,
+    :'Information and Communication Technology (ICT)', :'Integrated Science',
+    :'Liberal Studies',
+    :Mathematics, :'Mathematics Module 1', :'Mathematics Module 2', :Music,
+    :'Physical Education', :Physics,
+    :'Technology and Living', :'Tourism and Hospitality Studies',
+    :'Visual Arts'
+  ]
 end
