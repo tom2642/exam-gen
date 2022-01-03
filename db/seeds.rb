908ef10820dd14460cd30f1c18376eb5408518dc
Tom Tsui <tomtsui2642@gmail.com>
2022-01-03 23:27:10 +0900

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ billy = User.create!(
 
 billy.questions.create!(
   [
-    multiple_choice_question_or_short_question_text:
+    question:
       ['Which of the following factors will affect the demand for Samsung smartphones?',
        '(1)	A change in people’s income',
        '(2)	A change in the price of iPhones',
@@ -25,26 +25,26 @@ billy.questions.create!(
              'B.	(2) and (3) only',
              'C.	(3) and (4) only',
              'D.	(1), (2), (3) and (4)'],
-    correct_choice: 'A',
+    answer: 'A',
     score: 1,
     grade: 12,
     subject: :Economics
   ],
   [
-    multiple_choice_question_or_short_question_text:
+    question:
       ['Suppose the demand curve for a tour package to Korea is downward sloping. ' \
        'Which of the following CANNOT be constant when we move from one point to another on the demand curve?'],
     choice: ['A.	The price of the tour package to Korea',
              'B.	The price of a tour package to Japan',
              'C.	People’s income',
              'D.	The price of a travel guidebook to Korea'],
-    correct_choice: 'A',
+    answer: 'A',
     score: 1,
     grade: 12,
     subject: :Economics
   ],
   [
-    multiple_choice_question_or_short_question_text:
+    question:
       ['After changing to a better paid job, Josie eats spaghetti more frequently but pizza less frequently. To Josie,',
        '(1)	spaghetti is a normal good.',
        '(2)	pizza is an inferior good.',
@@ -53,7 +53,7 @@ billy.questions.create!(
              'B.	(1) and (3) only',
              'C.	(2) and (3) only',
              'D.	(1), (2) and (3)'],
-    correct_choice: 'A',
+    answer: 'A',
     score: 1,
     grade: 12,
     subject: :Economics
