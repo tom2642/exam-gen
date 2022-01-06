de490792147b021f4af0d728361f5d530f7b552d
Tom Tsui <tomtsui2642@gmail.com>
2022-01-06 21:49:10 +0900

    generated QuestionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  get 'questions/new'
+  get 'questions/create'
+  get 'questions/show'
+  get 'questions/edit'
+  get 'questions/update'
+  get 'questions/destroy'
   devise_for :users
   root to: 'pages#home'
   resource :question
