1e5867381e9e9e96a92dffb259a0a02445ba4a46
Tom Tsui <tomtsui2642@gmail.com>
2022-01-19 19:54:02 +0900

    question model not done

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,23 +1,7 @@
 class Question < ApplicationRecord
-  belongs_to :user
+  belongs_to :subjects
   has_many_attached :images
 
-  validates :question, :answer, :grade, :subject,
+  validates :question, :answer,
             presence: true
-  validates :grade, numericality: { in: 1..15 }
-
-  enum subject: [
-    :Biology, :'Business, Accounting and Financial Studies',
-    :Chemistry, :Chinese, :'Chinese History', :'Chinese Literature', :'Combined Science',
-    :'Design and Applied Technology',
-    :Economics, :English, :'English Literature', :'Ethics and Religious Studies',
-    :Geography,
-    :'Health Management and Social Care', :History,
-    :'Information and Communication Technology (ICT)', :'Integrated Science',
-    :'Liberal Studies',
-    :Mathematics, :'Mathematics Module 1', :'Mathematics Module 2', :Music,
-    :'Physical Education', :Physics,
-    :'Technology and Living', :'Tourism and Hospitality Studies',
-    :'Visual Arts'
-  ]
 end
