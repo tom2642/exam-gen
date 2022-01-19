1bfaccb3fe2440bb5cb2d8583bfdfe135ada5188
Tom Tsui <tomtsui2642@gmail.com>
2022-01-19 19:52:43 +0900

    subject model association

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,22 @@
 class Subject < ApplicationRecord
   belongs_to :user
+
+  validates :grade, :name,
+            presence: true
+  validates :grade, numericality: { in: 1..15 }
+
+  enum name: [
+    :Biology, :'Business, Accounting and Financial Studies',
+    :Chemistry, :Chinese, :'Chinese History', :'Chinese Literature', :'Combined Science',
+    :'Design and Applied Technology',
+    :Economics, :English, :'English Literature', :'Ethics and Religious Studies',
+    :Geography,
+    :'Health Management and Social Care', :History,
+    :'Information and Communication Technology (ICT)', :'Integrated Science',
+    :'Liberal Studies',
+    :Mathematics, :'Mathematics Module 1', :'Mathematics Module 2', :Music,
+    :'Physical Education', :Physics,
+    :'Technology and Living', :'Tourism and Hospitality Studies',
+    :'Visual Arts'
+  ]
 end
