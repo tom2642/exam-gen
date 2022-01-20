6f119ff189a34fa3241e976a7721a42c3651c08c
Tom Tsui <tomtsui2642@gmail.com>
2022-01-20 21:34:21 +0900

    docx upload form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,16 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Subject.find(params(:subject_id)).questions
   end
 
   def new
+    skip_authorization
   end
 
   def create
+    @question = Question.new
+    authorize @question
+
+    redirect_to new_subject_question_path
   end
 end
--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -1,7 +1,7 @@
 class QuestionPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where
     end
   end
 
