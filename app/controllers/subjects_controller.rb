98ae40e57703a45a4bbe222bd452b0c3691049ba
Tom Tsui <tomtsui2642@gmail.com>
2022-01-20 20:15:42 +0900

    subject index destroy create done

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,10 +1,27 @@
 class SubjectsController < ApplicationController
   def index
+    @subjects = policy_scope(Subject)
+    @new_subject = Subject.new
   end
 
   def create
+    @subject = Subject.new(subject_params)
+    @subject.user = current_user
+    authorize @subject
+    @subject.save!
+    redirect_to dashboard_path
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    authorize @subject
+    @subject.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:grade, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'subjects#index', as: 'dashboard'
-  resource :subjects, only: %i[create destroy] do # TODO: update
-    resource :questions, only: %i[index new create] # TODO: edit update destroy
+  resources :subjects, only: %i[create destroy] do # TODO: update
+    resources :questions, only: %i[index new create] # TODO: edit update destroy
   end
 end
