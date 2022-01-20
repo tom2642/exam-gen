077e4aa79b3239cc6f082b8c4006f1dd34ed0572
Tom Tsui <tomtsui2642@gmail.com>
2022-01-20 18:09:11 +0900

    gen-ed controller and view for subjects and questions

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -15,24 +15,4 @@ class QuestionsControllerTest < ActionDispatch::IntegrationTest
     get questions_create_url
     assert_response :success
   end
-
-  test "should get show" do
-    get questions_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get questions_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get questions_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get questions_destroy_url
-    assert_response :success
-  end
 end
---
+++ test/controllers/subjects_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class SubjectsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get subjects_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get subjects_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get subjects_destroy_url
+    assert_response :success
+  end
+end
