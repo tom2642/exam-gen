49c94fa2832ad37ea46e7d1ca1494cbb22dec29c
Tom Tsui <tomtsui2642@gmail.com>
2022-01-22 22:43:46 +0900

    parse topic into questions

--- app/controllers/concerns/parseable.rb
+++ app/controllers/concerns/parseable.rb
@@ -3,24 +3,27 @@ require 'pandoc-ruby'
 
 module Parseable
   def parse(uploaded_file)
+    # write the tmp file io object to a file
     File.open(Rails.root.join('tmp', 'docx', uploaded_file.original_filename), 'wb') do |file|
-      file.write(uploaded_file.read) # write the tmp file io object to a file
+      file.write(uploaded_file.read)
     end
 
+    # convert the docx into string(markdown) and split into seperate questions
     raw_strings = PandocRuby.convert(["tmp/docx/#{uploaded_file.original_filename}"], '--from=docx', '--to=markdown', '--extract-media=tmp/')
                             .split(/Question code: .+\n\n/)
-    raw_strings.delete_at(0) # seperate questions
+    topic = raw_strings.first.gsub(/\*\*Chapter \d+ /, '').gsub(/\*\*[\s\S]+/, '')
+    raw_strings.delete_at(0) # delete the part before the first question
     # raw_string #=> Question...\n\nA. Choice...\n\nAnswer:\n\nB\n\n....
     File.delete(Rails.root.join('tmp', 'docx', uploaded_file.original_filename)) # delete the docx
 
     results = []
     raw_strings.each_with_index do |raw_string, index|
-      # seperate images into different folder
+      # seperate images into different folder, each folder for one question
       if %r{.*!\[]\(tmp.*}.match?(raw_string)
         FileUtils.mkdir_p("tmp/media/#{index}") # create a folder for every questions that have images
         raw_string.scan("![](tmp").size.times do
           # For question having n images, move the first n images in media/ to media/#{index}/
-          first_image = Dir["tmp/media/*"].select{ |path| path.include?(".") }[0] # only select the files
+          first_image = Dir["tmp/media/*"].select { |path| path.include?("image") }.min # only select the files
           FileUtils.mv first_image, "tmp/media/#{index}/#{first_image.gsub('tmp/media/', '')}"
         end
       end
@@ -32,7 +35,7 @@ module Parseable
       question = question_and_choices.first.strip.gsub('> ', '').gsub("\n>\n", "\n\n").split("\n\n") # strip unwanted markdown code
       question.each { |line| line.gsub!(/\*\*/, '') unless line.include?('-------') } # remove bold unless table
       question[0] = "#{question[0]}\n\n" # first line of quesiton, \n\n = next line
-      question[1..-1].each_with_index { |line, i| question[i + 1] = "    #{line}\n\n" } # 4 leading spaces = list item
+      question[1..].each_with_index { |line, i| question[i + 1] = "    #{line}\n\n" } # 4 leading spaces = list item
       question = question.join
 
       # Parse choices
@@ -45,7 +48,7 @@ module Parseable
       end
 
       answer = splited_strings[1][0] # parse answer
-      results.push({ question: question, choices: choices, answer: answer })
+      results.push({ question: question, choices: choices, answer: answer, topic: topic })
     end
     return results
   end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,23 +11,28 @@ class QuestionsController < ApplicationController
 
   def create
     results = parse(params[:docx])
+    topic = Topic.new(name: results.first[:topic])
+    topic.valid? ? topic.save! : topic = Topic.where(name: results.first[:topic]).first
     results.each_with_index do |result, index|
+      result[:topic] = topic
       question = Question.new(result)
       question.subject = Subject.find(params[:subject_id])
       authorize question
       question.save! # save to get id
-      unless Dir["tmp/media/#{index}"].empty?
-        Dir["tmp/media/#{index}/*"].each do |fname| # image1.jpg -> #{question.id}_1.jpg
+
+      # for all the images inside the folder, attach imagen.jpg as #{question.id}_n.jpg to question
+      unless Dir["tmp/media/#{index}"].empty? # the question doesn't have a image
+        Dir["tmp/media/#{index}/*"].sort.each do |fname|
           question.images.attach(io: File.open(fname), filename: fname.gsub(%r{tmp/media/./image}, "#{question.id}_"))
         end
       end
+
+      # change the markdown in question that indicates the path of its images
       question.question.gsub!(%r{!\[]\(tmp//media/image}, "![](tmp//media/#{question.id}_")
-      question.choices&.map! do |choice|
-        choice.gsub(%r{!\[]\(tmp//media/image}, "![](tmp//media/#{question.id}_")
-      end
       question.save!
     end
 
+    FileUtils.rm_rf(Dir['tmp/media/*']) # delete local images
     redirect_to new_subject_question_path
   end
 end
