14180ebe4a0304d0a9f82b7f37539b79de999f4f
Tom Tsui <tomtsui2642@gmail.com>
2022-03-15 14:46:14 +0900

    add comments to html_convertable.rb

--- app/controllers/concerns/html_convertable.rb
+++ app/controllers/concerns/html_convertable.rb
@@ -2,12 +2,14 @@
 require 'pandoc-ruby'
 
 module HtmlConvertable
+  # transform markdown to html in order to display question previews in user's browser
   def md_to_html(questions)
     questions_and_htmls = []
     questions.each do |question|
       questions_and_htmls.push(
         {
           question: question,
+          # combine the :question and :choices columns into one html
           html: PandocRuby.convert("#{question[:question].join("\n\n")}\n\n#{question[:choices]&.join("\n\n")}",
                                    '--from=markdown', '--to=html')
         }
